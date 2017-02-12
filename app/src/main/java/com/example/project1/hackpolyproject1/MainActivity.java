fd3fd75b6ba6ceae2d14dfd3c128c9bbda01f372
stanleyr001 <stanleyr001@gmail.com>
2017-02-12 07:58:59 -0800

    Lots of errors. Needs to be fixed.

--- app/src/main/java/com/example/project1/hackpolyproject1/MainActivity.java
+++ app/src/main/java/com/example/project1/hackpolyproject1/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.project1.hackpolyproject1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,19 +11,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.client.FirebaseError;
+import com.firebase.client.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.firebase.client.Firebase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -35,6 +36,8 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String TAG = "TEST";
+
     ListView listView;
     static ArrayAdapter<String> arrayAdapter;
     static String value;
@@ -42,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView Mvalue;
     static ArrayList<String> tasks = new ArrayList<>();
     static Set<String> set;
+
+    /*
     public class User {
 
         public String name;
@@ -57,7 +62,33 @@ public class MainActivity extends AppCompatActivity {
             this.email = email;
         }
     }
+    */
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    //////////////////This is where my code starts//////////////////////
+    // Configure Google Sign In
+    //GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public EditText email;
+    public EditText password;
+
+
+    ///////////////////END OF MY CODE////////////////////////////
 
     //This is a comment added by Stanleyr001
 
@@ -70,10 +101,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        Button signIn = (Button) findViewById(R.id.button2);
-        Button login = (Button) findViewById(R.id.button3);
-        signIn.setOnClickListener(new View.OnClickListener() {
+
+        email = (EditText) findViewById(R.id.edittext_email);
+        password = (EditText) findViewById(R.id.edittext_password);
+
+
+
+        /////////////More code I added////////////////
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                Log.d(TAG, user.toString());
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+
+            };
+
             public void onClick(View view) {
                 //Intent i = new Intent(getApplicationContext(), MainPage.class);
                 //startActivity(i);
@@ -88,6 +138,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //test to pull from main.
+        final DatabaseReference mDatabase;
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
 
 
@@ -127,10 +179,127 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
+
+        //launch activity on button click;
+        Button signUp = (Button) findViewById(R.id.button_singup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createAccount(email.getText().toString(),password.getText().toString());
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getToken() instead.
+                    String uid = user.getUid();
+                } else {
+                    Log.d(TAG, "user=null");
+                }
+
+                //Intent i = new Intent(getApplicationContext(), MainPage.class);
+                //startActivity(i);
+            }
+        });
+
+        //launch activity on button click
+        Button login = (Button) findViewById(R.id.button_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                signIn(email.getText().toString(), password.getText().toString());
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getToken() instead.
+                    String uid = user.getUid();
+
+                //Intent i = new Intent(getApplicationContext(), MainPage.class);
+                //startActivity(i);
+                }
+            }
+        });
+
+    }
+
+    private void createAccount(String email, final String password) {
+        Log.d(TAG, "createAccount:" + email);
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG, "create fail");
+                            CharSequence text = "The account could not be created: " + task.getException();
+                            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                        } else {
+                            Log.d(TAG, "Success");
+                            Intent i = new Intent(getApplicationContext(), MainPage.class);
+                            startActivity(i);
+                        }
+
+                        // [START_EXCLUDE]
+
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            CharSequence text = "Log on Unsuccessful: " + task.getException();
+                            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+                        } else {
+                            Log.d(TAG, "Success");
+                            Intent i = new Intent(getApplicationContext(), MainPage.class);
+                            startActivity(i);
+                        }
+                    }
+                });
+        // [END sign_in_with_email]
     }
 
     public void Listitup(){
